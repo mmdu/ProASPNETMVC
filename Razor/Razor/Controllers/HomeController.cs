0271483d2a5021b8aa6b8bef802661364c9c173b
mingming du <dafengdumingming@outlook.com>
2017-08-07 21:53:52 -0500

    data-attribution

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        private Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Banana",
+            Description = "fruit imported",
+            Category = "Fruit",
+            Price = 11
+        };
+
+        public ActionResult NameandPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+            return View(myProduct);
+        }
+    }
+}
