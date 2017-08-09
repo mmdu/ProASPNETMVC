90701b0a97c64592f899f45917e0bc2845270f8a
mingming du <dafengdumingming@outlook.com>
2017-08-09 01:56:10 -0500

    Models

---
+++ EssentialTools6/EssentialTools6/Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;  
+using System.Web;
+
+namespace EssentialTools6.Models
+{
+    public class ShoppingCart
+    {
+        private LinqValueCalculator calc;
+                    
+        public ShoppingCart(LinqValueCalculator calcPara)
+        {
+            calc = calcPara;
+        }
+
+        public IEnumerable<Product> Products { get; set; }
+
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
