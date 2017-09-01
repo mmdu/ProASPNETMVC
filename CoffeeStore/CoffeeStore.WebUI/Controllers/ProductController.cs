51eb7a4905056d540cb8fcd2460d5d76be7fb41b
mingming du <dafengdumingming@outlook.com>
2017-09-01 03:50:54 -0500

    View Model, HTML helper Methode,View data, Page Links,

--- CoffeeStore/CoffeeStore.WebUI/Controllers/ProductController.cs
+++ CoffeeStore/CoffeeStore.WebUI/Controllers/ProductController.cs
@@ -5,20 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using CoffeeStore.Domain.Entities;
 using CoffeeStore.Domain.Abstract;
+using CoffeeStore.WebUI.Models;
 
 namespace CoffeeStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
         // GET: Product ,Depended on IProductRepository, need Ninject,
-        private IProductRepository repository;  
+        private IProductRepository repository;
+        public int PageSize = 4;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
-        public ActionResult List()
+        public ActionResult List(int page =1)
         {
-            return View(repository.Products);
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo =
+                    new PagingInfo
+                    {
+                        CurrrentPage = page,
+                        ItemPerPage = PageSize,
+                        TotalItems = repository.Products.Count()
+                    }
+            };
+
+            return View(model);
+            //return View(repository.Products.OrderBy(p=>p.ProductID).Skip((page-1)*PageSize).Take(PageSize));
         }
     }
 }
---
+++ CoffeeStore/CoffeeStore.WebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.Core.Smtp;
+using CoffeeStore.Domain.Entities;
+namespace CoffeeStore.WebUI.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
