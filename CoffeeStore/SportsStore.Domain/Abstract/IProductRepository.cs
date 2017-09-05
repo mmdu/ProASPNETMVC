4f5f035314478d70f520d1fbc9d99988f798218b
mingming du <dafengdumingming@outlook.com>
2017-09-05 13:24:45 -0500

    Database

--- CoffeeStore/SportsStore.Domain/Abstract/IProductRepository.cs
+++ CoffeeStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -9,3 +9,7 @@ namespace CoffeeStore.Domain.Abstract
     }
 
 }
+//This interface uses IEnumerable<T> to allow a caller to obtain a sequence of Product objects, without saying how
+//or where the data is stored or retrieved.A class that depends on the IProductRepository interface can obtain Product
+//objects without needing to know anything about where they are coming from or how the implementation class will
+//deliver them.This is the essence of the repository pattern
