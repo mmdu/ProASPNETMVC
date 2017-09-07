d14aa7cd90d0605f85625fe5371c44b49044f7ca
mingming du <dafengdumingming@outlook.com>
2017-09-06 23:49:47 -0500

    moq unit test 2

--- EssentialTools6/EssentialTools6.Tests/UnitTest2.cs
+++ EssentialTools6/EssentialTools6.Tests/UnitTest2.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EssentialTools6.Models;
 using  System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using Moq;
 namespace EssentialTools6.Tests
 {
@@ -24,7 +26,13 @@ namespace EssentialTools6.Tests
         //    var goalTotal = products.Sum(e => e.Price);
 
         {    // Arrange
+            //create a mock IDiscountHelper implementation
             Mock<IDiscountHelper> mock= new Mock<IDiscountHelper>();
+            //specify the way that MCOK behaves,Setup method to add a method to my mock object.
+            //When I call the Setup method, Moq passes me the interface that I have asked it to implement
+            //d ApplyDiscount method,  only method in the IDiscountHelper interface, and the method I need to test the
+            //LinqValueCalculator class.
+            //The It class defines a number of methods that are used with generic type parameters
             mock.Setup( m =>m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total =>total);
                      //lambda expression, Moq passes me a value of the type I receive in the ApplyDiscount
                      //I create a pass-through method , in which I return the value that is passed to the mock
@@ -33,13 +41,53 @@ namespace EssentialTools6.Tests
 
              var target=new LinqValueCalculator(mock.Object);
 
-                          //e Object property returns an implementation of the IDiscountHelper interface
+                          // Object property returns an implementation of the IDiscountHelper interface
                           //  where the ApplyDiscount method returns the value of the decimal parameter it is passed.
                   // action
            var result = target.ValueProducts(products);
 
             Assert.AreEqual(products.Sum(e=>e.Price), result);
 
+
+            //---------------------- Complex Example----------------------------------
+         
+
+
         }
+
+        private Product[] createProduct(decimal value)
+        {
+            return new[] { new Product { Price = value } };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Pass_through_Variable_Discount()
+        {
+            // arrange
+            Mock< IDiscountHelper> mock= new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(total => total);
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v==0))).Throws<System.ArgumentOutOfRangeException>();
+            mock.Setup(m => m.ApplyDiscount(It.Is<decimal>(v => v>100))).Returns<decimal>(total => (total*0.9M));
+            mock.Setup(m => m.ApplyDiscount(It.IsInRange<decimal>(10,100, Range.Inclusive))).Returns<decimal>(total => (total - 5));
+
+            var target = new LinqValueCalculator(mock.Object);
+
+            // act
+            decimal FiveDollarDiscount = target.ValueProducts(createProduct(5));
+            decimal TenDollarDiscount = target.ValueProducts(createProduct(10));
+            decimal FiftyDollarDiscount = target.ValueProducts(createProduct(50));
+            decimal HundredDollarDiscount = target.ValueProducts(createProduct(100));
+            decimal FiveHundredDollarDiscount = target.ValueProducts(createProduct(500));
+
+            // assert
+            Assert.AreEqual(5, FiveDollarDiscount, "$5 Fail");
+            Assert.AreEqual(5, TenDollarDiscount, "$10 Fail");
+            Assert.AreEqual(45, FiftyDollarDiscount, "$50 Fail");
+            Assert.AreEqual(95, HundredDollarDiscount, "$100 Fail");
+            Assert.AreEqual(450, FiveHundredDollarDiscount, "$500 Fail");
+            target.ValueProducts(createProduct(0));
+        }
+
     }
 }
